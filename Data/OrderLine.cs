09147a83f6cc075d77beb04ffe275444dd1e0161
oussama idbid <oussamaidbid@gmail.com>
2019-11-07 21:53:32 +0100

    fff

---
+++ Data/OrderLine.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace WebApplication1.Data
+{
+    public class OrderLine
+    {
+        public string name { get; set; }
+
+        public int amount { get; set; }
+
+        public double price { get; set; }
+
+    }
+}
