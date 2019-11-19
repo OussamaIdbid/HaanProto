bbeee46549fc2ced1e1eac3abfd270a5ee0104bd
oussama idbid <oussamaidbid@gmail.com>
2019-11-20 00:02:01 +0100

    as

--- Data/Product.cs
+++ Data/Product.cs
@@ -5,6 +5,7 @@ namespace WebApplication1.Data
         public static int Id { get; set; }
         public string Name{ get; set; }
         public double Price { get; set; }
+        public int CategoryId {get; set;}
 
 
     }
